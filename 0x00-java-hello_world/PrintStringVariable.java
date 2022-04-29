6e27f6398505aaed26fbfeb4dec9c393be4d2a7e
Paulo Grillo <paulodigitalart@gmail.com>
2022-04-29 16:21:43 -0300

    Create task 3 String

---
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -0,0 +1,8 @@
+public class PrintStringVariable {
+    public static void main(String[] args) {
+        String texto = "Holberton School";
+        System.out.println(texto);
+        System.out.println(texto);
+        System.out.println(texto.substring(0,9));
+    }
+}
