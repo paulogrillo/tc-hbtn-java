4009c02945607faad1411063824e7160e2a107bb
Paulo Grillo <paulodigitalart@gmail.com>
2022-04-29 16:35:31 -0300

    Create task 4 Concat

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,8 @@
+public class PrintStringConcat {
+    public static void main(String[] args) {
+        String texto1 = "Holberton";
+        String texto2 = "School";
+        String concat = texto1+" "+texto2;
+        System.out.printf("Bem vindo a "+concat+"!");
+    }
+}
