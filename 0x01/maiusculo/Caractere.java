33a0d08452cf792059294e25dd0a37554eca958c
Paulo Grillo <paulodigitalart@gmail.com>
2022-04-30 20:05:50 -0300

    Create task Caractere

---
+++ 0x01/maiusculo/Caractere.java
@@ -0,0 +1,9 @@
+public class Caractere {
+    public static boolean EhMaiusculo(char c){
+        if(c >= 'A' && c <= 'Z'){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
