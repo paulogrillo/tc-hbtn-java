fd2379cafbff6e196a0293a1361a2729631dc381
Paulo Grillo <53320819+paulogrillo@users.noreply.github.com>
2022-04-30 17:33:52 -0300

    Delete 0x01 directory

--- 0x01/numeros/Numeros0a99.java
+++
@@ -1,8 +0,0 @@
-public class Numeros0a99 {
-    public static void main(String[] args) {
-        int limite = 99;
-        for(int i = 0; i <= limite; i++) {
-            System.out.printf("%d, ", i);
-        }
-    }
-}
