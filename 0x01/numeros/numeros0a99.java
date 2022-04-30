6638c256804a9db200671f0ff78d5086a5afe99d
Paulo Grillo <paulodigitalart@gmail.com>
2022-04-30 16:35:26 -0300

    Create task 0

---
+++ 0x01/numeros/numeros0a99.java
@@ -0,0 +1,9 @@
+public class numeros0a99 {
+    public static void main(String[] args) {
+        int limite = 99;
+
+        for(int i = 0; i <= limite; i++) {
+            System.out.printf("%d, ", i);
+        }
+    }
+}
