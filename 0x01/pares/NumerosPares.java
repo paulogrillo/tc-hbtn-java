3d4fee1b3cebd023a5830e1df1d5a4342c61b5d2
Paulo Grillo <paulodigitalart@gmail.com>
2022-04-30 18:49:59 -0300

    fix

--- 0x01/pares/NumerosPares.java
+++ 0x01/pares/NumerosPares.java
@@ -1,7 +1,7 @@
 public class NumerosPares {
     public static void main(String[] args) {
-        int limit = 99;
-        for(int x= 0; x<limit; x++){
+        int limit = 96;
+        for(int x= 0;x<=limit; x++){
             if(x % 2 == 0){
                 System.out.printf("%d, ", x);
             }
