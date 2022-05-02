f330c77f95fc36f28a24599131b1b929e5f40f7a
Paulo Grillo <paulodigitalart@gmail.com>
2022-05-02 11:31:46 -0300

    fix

--- 0x02/divisao/Numero.java
+++ 0x02/divisao/Numero.java
@@ -1,14 +1,13 @@
 public class Numero {
-    static int result = 0;
+
     public static void dividir(int a, int b){
+        int result = 0;
         try{
              result = a / b;
-        }
-        catch (ArithmeticException e){
+        } catch (ArithmeticException e){
             System.out.println("Nao eh possivel dividir por zero");
-        }
-        finally {
-            System.out.println(a+" / "+b+" = "+result);
+        } finally {
+            System.out.println(a + " / " + b + " = " + result);
         }
     }
 }
