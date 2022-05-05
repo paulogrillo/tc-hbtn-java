917318cd6815d74ef2b699c6143defb661e3554e
Paulo Grillo <paulodigitalart@gmail.com>
2022-05-05 16:36:30 -0300

    fix

--- 0x04/status/PersonagemGame.java
+++ 0x04/status/PersonagemGame.java
@@ -5,20 +5,22 @@ public class PersonagemGame {
     private String status;
 
     public void tomarDano(int quantidadeDeDano) {
-        if(saudeAtual > 0) {
-            if (saudeAtual - quantidadeDeDano > 0) {
-                saudeAtual -= quantidadeDeDano;
-                setSaudeAtual(saudeAtual);
+        if(this.saudeAtual > 0) {
+            if (this.saudeAtual - quantidadeDeDano > 0) {
+                this.saudeAtual -= quantidadeDeDano;
+                setSaudeAtual(this.saudeAtual);
             } else {
                 setSaudeAtual(0);
             }
         }
+        setSaudeAtual(saudeAtual);
     }
     public void receberCura(int quantidadeDeCura) {
         this.saudeAtual = this.saudeAtual + quantidadeDeCura;
         if(this.saudeAtual > 100) {
             this.saudeAtual = 100;
         }
+        setSaudeAtual(this.saudeAtual);
     }
 
     public int getSaudeAtual() {
@@ -27,11 +29,11 @@ public class PersonagemGame {
 
     public void setSaudeAtual(int saudeAtual) {
         this.saudeAtual = saudeAtual;
-        if(this.saudeAtual == 0 || this.saudeAtual < 0){
-	this.status = "morto";
-	}if(this.saudeAtual > 0){
-		this.status = "vivo";
-	}
+        if(this.saudeAtual > 0){
+            this.status = "vivo";
+        }else{
+            this.status = "morto";
+        }
     }
 
     public String getNome() {
