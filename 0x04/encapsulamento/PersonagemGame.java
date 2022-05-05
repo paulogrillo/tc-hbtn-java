2e880726d4f260870fb028a66d50470e6a6ee083
Paulo Grillo <paulodigitalart@gmail.com>
2022-05-05 15:14:37 -0300

    Task encapsulamento

---
+++ 0x04/encapsulamento/PersonagemGame.java
@@ -0,0 +1,22 @@
+package encapsulamento;
+
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
