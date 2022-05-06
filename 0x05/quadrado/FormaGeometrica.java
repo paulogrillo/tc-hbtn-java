b101a25138f4af9a1e9c67c314375faa7e4f78f7
Paulo Grillo <paulodigitalart@gmail.com>
2022-05-06 16:13:07 -0300

    Task Quadrado

---
+++ 0x05/quadrado/FormaGeometrica.java
@@ -0,0 +1,6 @@
+public class FormaGeometrica {
+
+    public double area() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Funcao area nao implementada");
+    }
+}
