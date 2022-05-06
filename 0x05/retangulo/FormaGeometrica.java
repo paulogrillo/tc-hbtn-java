3573710ed4b60e496d191e0d2eb91d7d8b53e64b
Paulo Grillo <paulodigitalart@gmail.com>
2022-05-06 11:15:45 -0300

    Task Retangulo

---
+++ 0x05/retangulo/FormaGeometrica.java
@@ -0,0 +1,5 @@
+public class FormaGeometrica {
+    public double area() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Funcao area nao implementada");
+    }
+}
