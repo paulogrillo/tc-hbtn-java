5967fa260dd3b09e842f2f6c5c6fadc1332a8c23
Paulo Grillo <paulodigitalart@gmail.com>
2022-05-09 17:07:19 -0300

    Fix

--- 0x06/pedido_livraria/Program.java
+++ 0x06/pedido_livraria/Program.java
@@ -1,24 +1,23 @@
 import java.util.Locale;
-
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(new Locale("pt", "BR"));
-        Livro livro1 = new Livro("Duna", 2017, "Brasil", 56.30, 680, "Frank Herbert", 1);
-        Livro livro2 = new Livro("Um de nós está mentindo", 2018, "Brasil", 34.93, 384,
+        produtos.Livro livro1 = new produtos.Livro("Duna", 2017, "Brasil", 56.30, 680, "Frank Herbert", 1);
+        produtos.Livro livro2 = new produtos.Livro("Um de nós está mentindo", 2018, "Brasil", 34.93, 384,
                 "Karen Mcmanus", 1);
-        Livro livro3 = new Livro("Mindset Milionário", 2021, "Brasil", 31.70, 272,
+        produtos.Livro livro3 = new produtos.Livro("Mindset Milionário", 2021, "Brasil", 31.70, 272,
                 "José Roberto Marques", 1);
 
-        Dvd dvd1 = new Dvd("Anjos da Noite 5 - Guerras de Sangue", 2016, "Estados Unidos", 16.90,
+        produtos.Dvd dvd1 = new produtos.Dvd("Anjos da Noite 5 - Guerras de Sangue", 2016, "Estados Unidos", 16.90,
                 "Anna Foerster",
                 "Ação", 91);
-        Dvd dvd2 = new Dvd("Annabelle 2 - A Criação do Mal", 2017, "Brasil", 39.90,
+        produtos.Dvd dvd2 = new produtos.Dvd("Annabelle 2 - A Criação do Mal", 2017, "Brasil", 39.90,
                 "Jame Wan e Peter Safran",
                 "Terror",
                 109);
 
         // Pedido 1
-        ItemPedido[] itensPedido1 = new ItemPedido[]{
+        ItemPedido[] itensPedido1 = new ItemPedido[] {
                 new ItemPedido(livro1, 1),
                 new ItemPedido(dvd2, 1)
         };
@@ -28,7 +27,7 @@ public class Program {
         System.out.printf("Total Pedido: %.2f\n", pedido1.calcularTotal());
 
         // Pedido 2
-        ItemPedido[] itensPedido2 = new ItemPedido[]{
+        ItemPedido[] itensPedido2 = new ItemPedido[] {
                 new ItemPedido(livro1, 1),
                 new ItemPedido(dvd2, 1)
         };
@@ -38,7 +37,7 @@ public class Program {
         System.out.printf("Total Pedido: %.2f\n", pedido2.calcularTotal());
 
         // Pedido 3
-        ItemPedido[] itensPedido3 = new ItemPedido[]{
+        ItemPedido[] itensPedido3 = new ItemPedido[] {
                 new ItemPedido(livro2, 1),
                 new ItemPedido(livro3, 2),
                 new ItemPedido(dvd1, 1)
@@ -49,7 +48,7 @@ public class Program {
         System.out.printf("Total Pedido: %.2f\n", pedido3.calcularTotal());
 
         // Pedido 4
-        ItemPedido[] itensPedido4 = new ItemPedido[]{
+        ItemPedido[] itensPedido4 = new ItemPedido[] {
                 new ItemPedido(livro2, 1),
                 new ItemPedido(livro2, 1),
                 new ItemPedido(livro3, 2),
@@ -61,4 +60,6 @@ public class Program {
 
         System.out.printf("Total Pedido: %.2f\n", pedido4.calcularTotal());
     }
+
+
 }
