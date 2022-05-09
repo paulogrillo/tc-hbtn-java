ab8f76e4cc3293543df18090ba1f839a3651ee1b
Paulo Grillo <paulodigitalart@gmail.com>
2022-05-09 17:10:50 -0300

    Fix

--- 0x06/pedido_livraria/ItemPedido.java
+++ 0x06/pedido_livraria/ItemPedido.java
@@ -1,32 +1,28 @@
-
 import produtos.Produto;
+
 public class ItemPedido {
 
+    private Produto produto;
     private int quantidade;
 
-    private Produto produto;
-
     public ItemPedido(Produto produto, int quantidade) {
+        super();
         this.produto = produto;
         this.quantidade = quantidade;
     }
 
-    public ItemPedido() {
-    }
-
-    public int getQuantidade() {
-        return quantidade;
-    }
-
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
-    }
 
     public Produto getProduto() {
         return produto;
     }
-
     public void setProduto(Produto produto) {
         this.produto = produto;
     }
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
 }
--- 0x06/pedido_livraria/Pedido.java
+++ 0x06/pedido_livraria/Pedido.java
@@ -1,24 +1,38 @@
 public class Pedido {
 
     private double percentualDesconto;
-
     private ItemPedido[] itens;
+    private double total;
+
 
     public Pedido(int percentualDesconto, ItemPedido[] itens) {
+        super();
         this.percentualDesconto = percentualDesconto;
         this.itens = itens;
     }
 
     public double calcularTotal() {
-        double total = 0;
-
-        for (ItemPedido item : itens) {
-            total += item.getProduto().obterPrecoLiquido() * item.getQuantidade();
+        for (ItemPedido itemPedido : itens) {
+            total += (itemPedido.getProduto().obterPrecoLiquido(itemPedido.getProduto().getPrecoBruto())*itemPedido.getQuantidade());
         }
 
+        return total - (total * percentualDesconto / 100 );
+    }
 
-        total -= total * percentualDesconto / 100;
 
-        return total;
+    public double getPercentualDesconto() {
+        return percentualDesconto;
+    }
+
+    public void setPercentualDesconto(int percentualDesconto) {
+        this.percentualDesconto = percentualDesconto;
+    }
+
+    public ItemPedido[] getItens() {
+        return itens;
+    }
+
+    public void setItens(ItemPedido[] itens) {
+        this.itens = itens;
     }
 }
