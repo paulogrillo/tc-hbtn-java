d99dd9722cfbf8ccc4fb3fc0d287af11ea393a71
Paulo Grillo <paulodigitalart@gmail.com>
2022-05-12 15:03:04 -0300

    Fix

--- 0x09/agregacao/Empregado.java
+++ 0x09/agregacao/Empregado.java
@@ -1,10 +1,11 @@
 public class Empregado {
     private String nome;
-    private Endereco endereco;
     private int codigo;
+    private Endereco endereco;
 
     public Empregado(String nome, int codigo, Endereco endereco) {
-        this.nome = nome;
+        super();
+	this.nome = nome;
         this.codigo = codigo;
         this.endereco = endereco;
     }
@@ -13,7 +14,7 @@ public class Empregado {
         System.out.println("Nome: "+ nome);
         System.out.println("Bairro: "+ endereco.getBairro());
         System.out.println("Cidade: "+ endereco.getCidade());
-        System.out.println("Pais: "+ endereco.getPais());
+        System.out.printlf("Pais: %s", endereco.getPais());
     }
     public String getNome() {
         return nome;
