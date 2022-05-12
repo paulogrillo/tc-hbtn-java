77e331e09e74afd50c49c81274a0233149471531
Paulo Grillo <paulodigitalart@gmail.com>
2022-05-12 15:22:57 -0300

    Fix

--- 0x09/agregacao/Empregado.java
+++ 0x09/agregacao/Empregado.java
@@ -9,13 +9,15 @@ public class Empregado {
         this.codigo = codigo;
         this.endereco = endereco;
     }
+    
     public void apresentar(){
-        System.out.println("Codigo: "+this. codigo);
-        System.out.println("Nome: "+ this.nome);
-        System.out.println("Bairro: "+ this.endereco.getBairro());
-        System.out.println("Cidade: "+ this.endereco.getCidade());
-        System.out.printlf("Pais: %s", this.endereco.getPais());
+        System.out.println("Codigo: " + this.codigo);
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Bairro: " + this.endereco.getBairro());
+        System.out.println("Cidade: " + this.endereco.getCidade());
+        System.out.printf("Pais: " + this.endereco.getPais());
     }
+
     public String getNome() {
         return nome;
     }
