528a30b7a9fefc93c1942b6a87bdd2db3d6a25e6
Paulo Grillo <paulodigitalart@gmail.com>
2022-05-17 17:45:32 -0300

    Task Cookies

---
+++ List/cookies/PedidoCookie.java
@@ -0,0 +1,26 @@
+
+public class PedidoCookie {
+    private String sabor;
+    private int quantidadeCaixas;
+
+    public PedidoCookie(String sabor, int quantidadeCaixas) {
+        this.sabor = sabor;
+        this.quantidadeCaixas = quantidadeCaixas;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+    public int getQuantidadeCaixas() {
+        return quantidadeCaixas;
+    }
+
+    public void setQuantidadeCaixas(int quantidadeCaixas) {
+        this.quantidadeCaixas = quantidadeCaixas;
+    }
+}
