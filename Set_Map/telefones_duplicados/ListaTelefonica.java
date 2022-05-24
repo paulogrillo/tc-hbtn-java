0bcd43d46d0f9a008dab76007609fd20801c59f8
Paulo Grillo <paulodigitalart@gmail.com>
2022-05-24 17:27:52 -0300

    Fix

--- Set_Map/telefones_duplicados/ListaTelefonica.java
+++ Set_Map/telefones_duplicados/ListaTelefonica.java
@@ -2,54 +2,39 @@ import java.util.*;
 import java.util.Map.*;
 
 public class ListaTelefonica {
-    private HashMap<String, HashSet<Telefone>> listaTelefones;
-    public ListaTelefonica() {
-        listaTelefones = new HashMap<>();
-    }
+    private final HashMap<String, HashSet<Telefone>> lista = new HashMap<>();
 
-    public HashSet<Telefone> adicionarTelefone(String nome, Telefone telefone) {
-        HashSet<Telefone> tel = new HashSet<Telefone>();
-        boolean achou = false;
+    public void adicionarTelefone(String nome, Telefone telefone) {
+        HashSet<Telefone> telefones = new HashSet<>();
+        boolean duplicado = false;
         String chave = "";
 
 
-        for (Entry<String, HashSet<Telefone>> entry : listaTelefones.entrySet()) {
+        for (Entry<String, HashSet<Telefone>> entry : lista.entrySet()) {
             if (entry.getValue().contains(telefone)) {
-                achou=true;
+                duplicado = true;
                 chave = entry.getKey();
             }
         }
 
-        if(achou == false && listaTelefones.containsKey(nome)) {
-            tel.addAll(listaTelefones.get(nome));
-            tel.add(telefone);
-            listaTelefones.put(nome, tel);
-
+        if (!duplicado && lista.containsKey(nome)) {
+            telefones.addAll(lista.get(nome));
+            telefones.add(telefone);
+            lista.put(nome, telefones);
         }
 
-        if(chave.equals(nome) && achou) {
+        if (chave.equals(nome) && duplicado) {
             throw new IllegalArgumentException("Telefone jah existente para essa pessoa");
-        }else if(!chave.equals(nome) && achou){
+        } else if (!chave.equals(nome) && duplicado) {
             throw new IllegalArgumentException("Telefone jah pertence a outra pessoa");
         }
 
-        tel.add(telefone);
-        listaTelefones.put(nome, tel);
-
-        return tel;
-
+        telefones.add(telefone);
+        lista.put(nome, telefones);
     }
 
     public HashSet<Telefone> buscar(String nome) {
-
-        HashSet<Telefone> telTemp = new HashSet<Telefone>();
-
-        if (!listaTelefones.containsKey(nome)) {
-            return null;
-        } else {
-            telTemp = listaTelefones.get(nome);
-            return telTemp;
-        }
+        return this.lista.get(nome);
     }
 }
 
