41c184b04920c764fe914c97d583366472efe559
Paulo Grillo <paulodigitalart@gmail.com>
2022-05-31 11:23:28 -0300

    Fix

--- Streams_Collectors_Optional/maior_preco/Program.java
+++ Streams_Collectors_Optional/maior_preco/Program.java
@@ -3,7 +3,7 @@ import java.util.Locale;
 
 public class Program {
     public static void main(String[] args) {
-        Locale.setDefault(new Locale("pt","BR"));
+	Locale.setDefault(new Locale("pt","BR"));
         Produto produto1 = new Produto(1,
                 "Carro Vortex", CategoriaProduto.BRINQUEDO, 799.90);
 
@@ -28,14 +28,15 @@ public class Program {
 
         Pedido pedido1 = new Pedido(1, List.of(produto4, produto3, produto6), cliente1);
         Pedido pedido2 = new Pedido(2, List.of(produto1, produto6, produto2), cliente2);
-        Pedido pedido3 = new Pedido(3, List.of(produto4, produto6, produto2, produto5), cliente3);
+        Pedido pedido3 = new Pedido(3, List.of(produto4, produto6, produto5), cliente3);
 
-        List<Produto> produtosLivro1 = Consulta.obterLivrosDoPedido(pedido1);
-        List<Produto> produtosLivro2 = Consulta.obterLivrosDoPedido(pedido2);
-        List<Produto> produtosLivro3 = Consulta.obterLivrosDoPedido(pedido3);
+        Produto produtoMaior1 = Consulta.obterProdutoMaiorPreco(pedido1.getProdutos());
+        Produto produtoMaior2 = Consulta.obterProdutoMaiorPreco(pedido2.getProdutos());
+        Produto produtoMaior3 = Consulta.obterProdutoMaiorPreco(pedido3.getProdutos());
 
-        System.out.println(produtosLivro1);
-        System.out.println(produtosLivro2);
-        System.out.println(produtosLivro3);
+        System.out.println(produtoMaior1);
+        System.out.println(produtoMaior2);
+        System.out.println(produtoMaior3);
     }
 }
+
