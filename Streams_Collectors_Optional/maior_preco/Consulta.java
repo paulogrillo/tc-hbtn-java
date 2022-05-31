4d58324c59bf44d2c770257273d406c74ab8f06b
Paulo Grillo <paulodigitalart@gmail.com>
2022-05-31 11:30:09 -0300

    Fix

--- Streams_Collectors_Optional/maior_preco/Consulta.java
+++ Streams_Collectors_Optional/maior_preco/Consulta.java
@@ -1,4 +1,7 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Consulta {
     //método estático obterLivrosDoPedido
