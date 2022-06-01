4cc89246e9e9662d6b266c1681c64db7c8847d05
Paulo Grillo <paulodigitalart@gmail.com>
2022-06-01 15:44:22 -0300

    Task Exercicios Fix

---
+++ Streams_Collector_Optional_exercicios/quantidade_pessoas_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> listaDePessoas) {
+       return listaDePessoas.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo,
+			() -> new TreeMap<>(Comparator.reverseOrder()),
+                        Collectors.toCollection(TreeSet::new)));
+
+    }
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> todasPessoas) {
+        return todasPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo,
+                Collectors.counting()));
+    }
+}
