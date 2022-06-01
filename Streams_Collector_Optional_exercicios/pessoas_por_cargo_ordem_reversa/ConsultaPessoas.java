4a99e1de9dda2279612065952bcf8a77b85cb38a
Paulo Grillo <paulodigitalart@gmail.com>
2022-06-01 11:55:15 -0300

    Task exercicios fix

---
+++ Streams_Collector_Optional_exercicios/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> listaDePessoas) {
+       return listaDePessoas.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo,
+			() -> new TreeMap<>(Comparator.reverseOrder()),
+                        Collectors.toCollection(TreeSet::new)));
+
+    }
+}
